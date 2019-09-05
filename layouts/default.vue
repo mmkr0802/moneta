fd08b97ee64de6612e855106f7b8d03687b1079e
yamaki <yamaki@google.com>
2019-09-05 11:08:48 +0900

    ブラウザバックを直しました

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,7 +12,10 @@
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-btn class="title" nuxt to="login" text>{{ account | name }}様</v-btn>
+        <v-btn class="title" nuxt @click="logoff" text>{{ account | name }}様</v-btn>
+              
+
+
       </v-toolbar-items>
     </v-app-bar>
     <v-content>
@@ -34,5 +37,11 @@ export default {
   computed: {
     ...mapGetters("login", ["account", "branch", "bank"]),
   },
+  methods: {
+    logoff() {
+      this.$router.push("/login");
+      this.$store.dispatch("login/id", null);
+    }
+  }
 };
 </script>
